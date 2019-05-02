bcea67cc686f579a3904e2cfbe3a5a805fb74c8f
Uggla <uggla@free.fr>
2019-05-02 23:00:15 +0200

    Fix install that need root rights

--- install.sh
+++ install.sh
@@ -49,7 +49,8 @@ install_tools_via_git() {
 
 install_tools_via_curl() {
   if [[ -f /usr/local/bin/bfg-1.13.0.jar ]]; then
-    curl https://repo1.maven.org/maven2/com/madgag/bfg/1.13.0/bfg-1.13.0.jar \
+    sudo curl \
+      https://repo1.maven.org/maven2/com/madgag/bfg/1.13.0/bfg-1.13.0.jar \
       -o /usr/local/bin/bfg-1.13.0.jar
   fi
 }
