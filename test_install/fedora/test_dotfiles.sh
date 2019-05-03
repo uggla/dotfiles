dcf112f67241b047937a59d2c91b272489c795c6
Uggla <uggla@free.fr>
2019-05-04 01:21:07 +0200

    Rename container name

--- test_install/fedora/test_dotfiles.sh
+++ test_install/fedora/test_dotfiles.sh
@@ -4,10 +4,10 @@ IFS=$'\n\t'
 
 date +%s >RSTCACHE
 
-docker build -t dotfiles_tests .
-if docker ps -a | awk '{print $NF}' | grep dotfiles_tests; then
-  docker rm -f dotfiles_tests
+docker build -t test_dotfiles_fedora .
+if docker ps -a | awk '{print $NF}' | grep test_dotfiles_fedora; then
+  docker rm -f test_dotfiles_fedora
 fi
-docker run --name="dotfiles_tests" -id dotfiles_tests bash
-docker exec -ti dotfiles_tests bash -c "cd dotfiles && make all"
-docker exec -ti dotfiles_tests bash -c "find . -type d -name tests -exec bats {} \;"
+docker run --name="test_dotfiles_fedora" -id test_dotfiles_fedora bash
+docker exec -ti test_dotfiles_fedora bash -c "cd dotfiles && make all"
+docker exec -ti test_dotfiles_fedora bash -c "find . -type d -name tests -exec bats {} \;"
