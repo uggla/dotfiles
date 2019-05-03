c6a720b83f4b4f5a51ea343d3234f4c3831f2355
Uggla <uggla@free.fr>
2019-05-04 01:18:06 +0200

    Add missing tests for test_install

--- test_install/fedora/test_dotfiles.sh
+++ test_install/fedora/test_dotfiles.sh
@@ -10,4 +10,4 @@ if docker ps -a | awk '{print $NF}' | grep dotfiles_tests; then
 fi
 docker run --name="dotfiles_tests" -id dotfiles_tests bash
 docker exec -ti dotfiles_tests bash -c "cd dotfiles && make all"
-docker exec -ti dotfiles_tests bash -c "cd dotfiles && bats bash/tests"
+docker exec -ti dotfiles_tests bash -c "find . -type d -name tests -exec bats {} \;"
